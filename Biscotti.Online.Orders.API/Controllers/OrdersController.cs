afc57b13d3e8a684df75068c11975f564ff3f8df
Matthew Morgan <mmorgan@nerdunited.com>
2023-03-16 00:27:04 -0600

    Got state stores working in sql server

--- Biscotti.Online.Orders.API/Controllers/OrdersController.cs
+++ Biscotti.Online.Orders.API/Controllers/OrdersController.cs
@@ -1,6 +1,10 @@
 using Biscotti.Online.Orders.Abstract;
+using Biscotti.Online.Orders.API.State;
+using Biscotti.Online.Reservations.Abstract;
+using Dapr;
 using Dapr.Client;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -10,6 +14,14 @@ namespace Biscotti.Online.Orders.API.Controllers;
 [Route("[controller]")]
 public class OrdersController : ControllerBase
 {
+    private const string StoreName = "orders-sqlstore";
+    private readonly ILogger<OrdersController> _logger;
+
+    public OrdersController(ILogger<OrdersController> logger)
+    {
+        _logger = logger;
+    }
+
     /// <summary>
     /// Method for submitting a new order.
     /// </summary>
@@ -17,23 +29,48 @@ public class OrdersController : ControllerBase
     /// <param name="daprClient">State client to interact with Dapr runtime.</param>
     /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
     [HttpPost]
-    public async Task<ActionResult<Guid>> SubmitOrder(Order order, [FromServices] DaprClient daprClient)
+    public async Task<ActionResult<Guid>> Post([FromBody] Order order, [FromServices] DaprClient daprClient)
     {
-        Console.WriteLine("Enter submit order");
+        _logger.LogInformation("Enter submit order");
 
         order.Id = Guid.NewGuid();
 
+        var state = await daprClient.GetStateEntryAsync<OrderState>(StoreName, order.Id.ToString());
+        state.Value ??= new OrderState
+        {
+            CreatedOn = DateTime.UtcNow,
+            UpdatedOn = DateTime.UtcNow,
+            Order = order
+        };
+
         foreach (var item in order.Items)
         {
-            /* a dynamic type is passed to sample.microservice.reservation and not
-            the Order in scope of sample.microservice.order, you could use DTO instead */
-            var data = new { sku = item.ProductCode, quantity = item.Quantity };
-            var result = await daprClient.InvokeMethodAsync<object, dynamic>(HttpMethod.Post, "reservations-api", "reservations", data);
-            Console.WriteLine($"sku: {result.GetProperty("value").GetProperty("sku")} === new quantity: {result.GetProperty("value").GetProperty("quantity")}");
+            var data = new Item
+            {
+                SKU = item.ProductCode,
+                Quantity = item.Quantity
+            };
+            var result = await daprClient.InvokeMethodAsync<Item, Item>(HttpMethod.Post, "reservations-api", "reservations", data);
+            _logger.LogInformation("sku: {SKU} === new quantity: {Quantity}", result.SKU, result.Quantity);
         }
 
-        Console.WriteLine($"Submitted order {order.Id}");
+        await state.SaveAsync();
 
-        return order.Id;
+        _logger.LogInformation("Submitted order {Id}", order.Id);
+
+        return Ok(order.Id);
+    }
+
+    [HttpGet("{state}")]
+    public ActionResult<Order> Get([FromState(StoreName)] StateEntry<OrderState> state)
+    {
+        _logger.LogInformation("Enter order retrieval");
+        if (state.Value is null)
+        {
+            return NotFound();
+        }
+        var result = state.Value.Order;
+        _logger.LogInformation("Retrieved order {Id}", result.Id);
+        return Ok(result);
     }
 }
--- Biscotti.Online.Reservations.API/Controllers/ReservationsController.cs
+++ Biscotti.Online.Reservations.API/Controllers/ReservationsController.cs
@@ -1,14 +1,20 @@
 using Biscotti.Online.Reservations.Abstract;
+using Biscotti.Online.Reservations.API.State;
+using Dapr;
 using Dapr.Client;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Biscotti.Online.Reservations.API.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class ReservationsController : ControllerBase
 {
+    private const string StoreName = "reservations-sqlstore";
+
     private readonly ILogger<ReservationsController> _logger;
 
     public ReservationsController(ILogger<ReservationsController> logger)
@@ -17,21 +23,67 @@ public class ReservationsController : ControllerBase
     }
 
     [HttpPost]
-    public IResult SubmitReservation([FromServices] DaprClient client, [FromBody] Item item)
+    public async Task<ActionResult<Item>> Post([FromBody] Item item, [FromServices] DaprClient client)
     {
         _logger.LogInformation("Enter Reservation");
 
-        /* a specific type is used in sample.microservice.reservation and not
-        reused the class in sample.microservice.order with the same signature: 
-        this is just to not introduce DTO and to suggest that it might be a good idea
-        having each service separating the type for persisting store */
-        Item storedItem;
-        storedItem = new Item();
-        storedItem.SKU = item.SKU;
-        storedItem.Quantity -= item.Quantity;
+        var state = await client.GetStateEntryAsync<ItemState>(StoreName, item.SKU);
+        state.Value ??= new ItemState
+        {
+            SKU = item.SKU,
+            Changes = new List<ItemReservation>()
+        };
 
-        _logger.LogInformation($"Reservation of {storedItem.SKU} is now {storedItem.Quantity}");
+        // Update balance
+        state.Value.BalanceQuantity -= item.Quantity;
 
-        return Results.Ok(storedItem);
+        // Record change
+        var change = new ItemReservation
+        {
+            SKU = item.SKU,
+            Quantity = item.Quantity,
+            ReservedOn = DateTime.UtcNow
+        };
+        state.Value.Changes ??= new List<ItemReservation>();
+        state.Value.Changes.Add(change);
+        if (state.Value.Changes.Count > 10)
+        {
+            state.Value.Changes.RemoveAt(0);
+        }
+
+        // TrySaveAsync leverages ETag while SaveAsync does not
+        _logger.LogInformation("ETag {ETag}", state.ETag);
+        var saved = await state.TrySaveAsync();
+        if (!saved)
+        {
+            _logger.LogError("Failed to save state");
+            return StatusCode(500);
+        }
+
+        // Return current balance
+        var result = new Item
+        {
+            SKU = state.Value.SKU,
+            Quantity = state.Value.BalanceQuantity
+        };
+        _logger.LogInformation($"Reservation of {result.SKU} is now {result.Quantity}");
+        return Ok(result);
+    }
+
+    [HttpGet("{state}")]
+    public ActionResult<Item> Get([FromState(StoreName)] StateEntry<ItemState> state)
+    {
+        _logger.LogInformation("Enter item retrieval");
+        if (state.Value is null)
+        {
+            return NotFound();
+        }
+        var result = new Item
+        {
+            SKU = state.Value.SKU,
+            Quantity = state.Value.BalanceQuantity
+        };
+        _logger.LogInformation("Retrieved {SKU} is {Quantity}", result.SKU, result.Quantity);
+        return Ok(result);
     }
 }
